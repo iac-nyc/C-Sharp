a51ebc5c522f7b50870e3d97f7c200039071badc
Iftekhar A Chowdhury <007ifte@gmail.com>
2019-06-27 03:09:32 -0400

    Interpolation

---
+++ Basics/Interpolation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+class Program
+{
+
+    // TODO: Defining a Quote method!
+   
+    public static string Quote(string text)
+    {
+       
+        //string a = "\"" + text + "\"";
+        string a = $"\"{ text } \"";
+
+
+        return a;
+    }
+   
+
+
+    static void Main(string[] args)
+    {
+        // Quote by Maya Angelou.
+        Console.WriteLine(Quote("When you learn, teach. When you get, give."));
+        // Quote by Benjamin Franklin.
+        Console.WriteLine(Quote("No gains without pains."));
+
+
+    }
+
+}
