0c333c139a618f1b1d2f7f746b2ebcb6f3444dcf
Iftekhar A Chowdhury <007ifte@gmail.com>
2019-08-03 21:08:44 -0400

    lambda expression

---
+++ Basics/lambda.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace FunctionalProgramming
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Action<string> sayGreeting;
+            
+            Func<string, string> conversate =  (message) =>
+            {
+                Console.WriteLine(message);
+                return Console.ReadLine();
+            };   
+            string input = conversate("What's your name?");
+            
+            sayGreeting = (greeting) =>
+            {
+                 Console.WriteLine(string.Format(greeting, input));
+            };    
+          
+            
+            sayGreeting("Hello, {0}");
+            conversate("Nice to see you!");
+            conversate("Are you doing well?");
+            sayGreeting("Later, {0}");
+           
+           
+        }
+    }
+}
