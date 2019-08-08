c0c035609a40b4af408e585d23e41ec4a0b7db60
Iftekhar A Chowdhury <007ifte@gmail.com>
2019-08-08 18:12:13 -0400

    Element Operator - Single

---
+++ Basics/LINQ_Element_Operator.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirdWatcher
+{
+    public class Bird
+    {
+        public Bird(string name, string color, int sightings)
+        {
+            Name = name;
+            Color = color;
+            Sightings = sightings;
+        }
+
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public int Sightings { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("Name: {0}, Color: {1}, Sightings: {2}\n", Name, Color, Sightings);
+        }
+    }
+
+    public static class BirdRepository
+    {
+        public static List<Bird> LoadBirds()
+        {
+            return new List<Bird>
+            {
+                new Bird ( "Cardinal", "Red", 3 ),
+                new Bird ( "Dove", "White", 2 ),
+                new Bird ( "Robin", "Red", 5 ),
+                new Bird ( "Canary", "Yellow", 0 ),
+                new Bird ( "Blue Jay", "Blue", 1 )
+            };
+        }
+    }
+}
+
+/*
+/////////////////////////
+/// Element Operators ///
+/////////////////////////
+
+ElementAt
+ElementAtOrDefault
+First
+FirstOrDefault
+Single
+SingleOrDefault
+Last 
+LastOrDefault
+
+Single<TSource>(IEnumerable<TSource>)
+
+Returns the only element of a sequence, and throws an exception if there is not exactly one element in the sequence.
+*/
+
+birds.Where(b => b.Name == "Dove").Single();
+birds.Single ( b => b.Name == "Dove");
