c2697523c7b5c397cc566e16b004a38e5b933190
Iftekhar A Chowdhury <007ifte@gmail.com>
2019-08-15 19:33:21 -0400

    LINQ - Join Operator

---
+++ Basics/LINQ_JOIN_Operator.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirdWatcher
+{
+    public class Bird
+    {
+        public Bird(string name, string color, int sightings)
+        {
+            Name = name;
+            Color = color;
+            Sightings = sightings;
+        }
+
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public int Sightings { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("Name: {0}, Color: {1}, Sightings: {2}\n", Name, Color, Sightings);
+        }
+    }
+
+    public static class BirdRepository
+    {
+        public static List<Bird> LoadBirds()
+        {
+            return new List<Bird>
+            {
+                new Bird ( "Cardinal", "Red", 3 ),
+                new Bird ( "Dove", "White", 2 ),
+                new Bird ( "Robin", "Red", 5 ),
+                new Bird ( "Canary", "Yellow", 0 ),
+                new Bird ( "Blue Jay", "Blue", 1 )
+            };
+        }
+    }
+}
+
+
+
+
+////////////////////
+////JOINS///////////
+////////////////////"
+
+var colors = new List<string> {"Red", "Blue", "Purple"};
+
+var favoriteBirds = from b in birds
+                    join c in colors on b.Color equals c
+                    select b;
+favoriteBirds;
+/*
+{
+Name: Cardinal, Color: Red, Sightings: 3,
+Name: Robin, Color: Red, Sightings: 5,
+Name: Blue Jay, Color: Blue, Sightings: 1
+}
+*/
+                    
+                    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
