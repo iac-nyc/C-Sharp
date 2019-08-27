2d2c2f7bc461bc28ff097ebc00c1937c804f2351
Iftekhar A Chowdhury <007ifte@gmail.com>
2019-08-27 16:45:31 -0400

    SelectMany GroupBy

--- Basics/BirdWatchers.cs
+++ Basics/BirdWatchers.cs
@@ -1,3 +1,5 @@
 birds.SelectMany(b => b.Sightings).Select(s => s.Place.Country);
 
 birds.SelectMany(b => b.Sightings).Select(s => s.Place.Country).Distinct();
+
+birds.SelectMany(b => b.Sightings).GroupBy(s => s.Place.Country).Select(grp => new { Country = grp.Key, Sightings = grp.Count()});
